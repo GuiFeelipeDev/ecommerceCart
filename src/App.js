ef9f6597428244cbaf4de608187067672dc8b386
GuiFeelipeDev <guifelipedev@gmail.com>
2022-04-13 16:52:16 -0400

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Hero from "./Hero";
 import Cart from "./Cart";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,6 +6,8 @@ import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import ItemsProvider, { CartProvider } from "./context/index";
 function App() {
+  localStorage.setItem("DB", "[]");
+  localStorage.setItem("QNT", "[]");
   return (
     <>
       <CartProvider>
